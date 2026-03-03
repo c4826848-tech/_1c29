782020a5524a231ebd96db678590a0140157f23c
c4826848-tech <c4826848@gmail.com>
2026-03-03 20:42:20 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDK8yUVMT33xvad8cHe56IDYnJ28Ht6gN0",
+  authDomain: "lyaly-ramadan.firebaseapp.com",
+  databaseURL: "https://lyaly-ramadan-default-rtdb.firebaseio.com/",
+  projectId: "lyaly-ramadan",
+  storageBucket: "lyaly-ramadan.firebasestorage.app",
+  messagingSenderId: "1072262325853",
+  appId: "1:1072262325853:web:6fa49120226f2c91178fb4"
+};
