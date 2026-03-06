2e7cf08325a36f41352bee39d2f50d44edad8aa6
c4826848-tech <c4826848@gmail.com>
2026-03-06 21:35:40 +0400

    Update teams.js

--- teams.js
+++ teams.js
@@ -4,12 +4,10 @@ window.DEFAULT_TEAMS = [
 {id:"T2",name:"الفريق 2",code:"2222",group:"A"},
 {id:"T3",name:"الفريق 3",code:"3333",group:"A"},
 {id:"T4",name:"الفريق 4",code:"4444",group:"A"},
-{id:"T5",name:"الفريق 5",code:"5555",group:"A"},
 
+{id:"T5",name:"الفريق 5",code:"5555",group:"B"},
 {id:"T6",name:"الفريق 6",code:"6666",group:"B"},
 {id:"T7",name:"الفريق 7",code:"7777",group:"B"},
-{id:"T8",name:"الفريق 8",code:"8888",group:"B"},
-{id:"T9",name:"الفريق 9",code:"9999",group:"B"},
-{id:"T10",name:"الفريق 10",code:"1010",group:"B"}
+{id:"T8",name:"الفريق 8",code:"8888",group:"B"}
 
 ];
