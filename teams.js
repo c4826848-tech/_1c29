45e6362cc4c0777fd63e8110855caefe2d119136
c4826848-tech <c4826848@gmail.com>
2026-03-06 17:09:25 +0400

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,50 +1,11 @@
 window.DEFAULT_TEAMS = [
-  {
-    "id": "t1",
-    "name": "فريق النور",
-    "code": "1133",
-    "group": "A"
-  },
-  {
-    "id": "t2",
-    "name": "فريق الهدى",
-    "code": "2244",
-    "group": "A"
-  },
-  {
-    "id": "t3",
-    "name": "فريق الفجر",
-    "code": "3355",
-    "group": "A"
-  },
-  {
-    "id": "t4",
-    "name": "فريق الإبداع",
-    "code": "4466",
-    "group": "A"
-  },
-  {
-    "id": "t5",
-    "name": "فريق البيان",
-    "code": "5577",
-    "group": "B"
-  },
-  {
-    "id": "t6",
-    "name": "فريق السحاب",
-    "code": "6688",
-    "group": "B"
-  },
-  {
-    "id": "t7",
-    "name": "فريق الصقور",
-    "code": "7799",
-    "group": "B"
-  },
-  {
-    "id": "t8",
-    "name": "فريق الكرام",
-    "code": "8800",
-    "group": "B"
-  }
+
+{id:"t1",name:"الفريق 1",group:"A",code:"1111"},
+{id:"t2",name:"الفريق 2",group:"A",code:"2222"},
+{id:"t3",name:"الفريق 3",group:"A",code:"3333"},
+
+{id:"t4",name:"الفريق 4",group:"B",code:"4444"},
+{id:"t5",name:"الفريق 5",group:"B",code:"5555"},
+{id:"t6",name:"الفريق 6",group:"B",code:"6666"}
+
 ];
